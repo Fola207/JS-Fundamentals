12d1d3b35452f88b4bc9037fb9828334ba7b1407
Oluwafolakunmi Israel <folarted@gmail.com>
2025-05-31 17:06:16 +0100

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,5 +1,3 @@
 const args = process.argv.slice(2);
-
 const firstArg = args.at(0); // Using at() instead of length
-
 console.log(firstArg ?? "No argument"); // Using nullish coalescing for readability
