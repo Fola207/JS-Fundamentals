ba809f877addfa75c1304824f1122cd1df69b8ef
Oluwafolakunmi Israel <folarted@gmail.com>
2025-05-31 17:10:52 +0100

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,11 +1,9 @@
 const args = process.argv.slice(2);
-
 const message = args.at(0) === undefined 
   ? "No argument" 
   : args.at(1) === undefined 
     ? "Argument found" 
     : "Arguments found";
-
 console.log(message);
 
 
